abc43a201a4010625c3864dd7421bbe89d803d29
handryhua <58896496+handryhua@users.noreply.github.com>
2026-04-07 13:07:40 +0700

    Refactor products data structure to use objects

--- data/products.js
+++ data/products.js
@@ -1,216 +1,336 @@
-[
 {
-  "sku":"6865",
- "productName":"ADAPTOR (BPM)",
- "price":98011,
- "category":"PART"
-},
-{
-  "sku":"B438",
- "productName":"BLOOD PRESSURE MONITOR HEM-6232T",
- "price":1081336,
- "category":"BPM"
-},
-{"sku":"D166",
- "productName":"BLOOD PRESSURE MONT HBP1120",
- "price":1554000,
- "category":"BPM"},
-{"sku":"D165",
- "productName":"BLOOD PRESSURE MONT HBP1320",
- "price":3108000,
- "category":"BPM"},
-{
-  "sku":"E040",
- "productName":"BLOOD PRESSURE MONT HBP9031C",
- "price":27454000,
- "category":"BPM"
-},
-{
-  "sku":"8085",
- "productName":"BLOOD PRESSURE MONT HEM 7120",
- "price":408000,
- "category":"BPM"
-},
-{
-  "sku":"D238",
- "productName":"BLOOD PRESSURE MONT HEM 7143T1",
- "price":491600,
- "category":"BPM"},
-{
-  "sku":"E250",
- "productName":"BLOOD PRESSURE MONT HEM 7156-AP",
- "price":577280,
- "category":"BPM"},
-{
-  "sku":"D526",
-  "productName":"BLOOD PRESSURE MONT HEM 7156T",
-  "price":619500,
-  "category":"BPM"},
-{
-  "sku":"D239",
-  "productName":"BLOOD PRESSURE MONT HEM 7156T-A",
-  "price":783000,
-  "category":"BPM"
-},
-{
-  "sku":"D163",
-  "productName":"BLOOD PRESSURE MONT HEM 907",
-  "price":7770000,
-  "category":"BPM"},
-{
-  "sku":"E658",
-  "productName":"BLOOD PRESSURE MONT HEM-7142T2",
-  "price":412000,
-  "category":"BPM"
-},
-{ 
-  "sku":"C332",
-  "productName":"BLOOD PRESSURE MONT HEM-7361T",
-  "price":1229500,
-  "category":"BPM"
-},
-{
-  "sku":"F140",
-  "productName":"BLOOD PRESSURE MONT HEM-7383T1",
-  "price":1230000,
-  "category":"BPM"
-},
-{
-  "sku":"E670",
-  "productName":"BLOOD PRESSURE MONT JPN-750",
-  "price":702512,
-  "category":"BPM"
-},
-{
-  "sku":"E457",
-  "productName":"BLOOD PRESSURE MONT+ECG HEM-7530T",
-  "price":2492440,
-  "category":"BPM"
-},
-{
-  "sku":"6830",
-  "productName":"BODY FAT MONITOR HBF-375",
-  "price":1641320,
-  "category":"BCM"
-},
-{
-  "sku":"6863",
-  "productName":"CUFF - L HEM-RML31-BAP",
-  "price":145449,
-  "category":"PART"
-},
-{
-  "sku":"6862",
-  "productName":"CUFF - M HEM-CR24-BAP",
-  "price":109238,
-  "category":"PART"
-},
-{
-  "sku":"6861",
-  "productName":"CUFF - S HEM-CS24-BAP",
-  "price":97100,
-  "category":"PART"
-},
-{
-  "sku":"8375",
-  "productName":"EAR THERMOMETER TH-8395",
-  "price":452153,
-  "category":"ETF"
-},
-{
-  "sku":"B292",
-  "productName":"NEBULIZER NEC 101",
-  "price":429250,
-  "category":"NEB"
-},
-{
-  "sku":"4682",
-  "productName":"NEBULIZER NE-C28",
-  "price":619500,
-  "category":"NEB"
-},
-{
-  "sku":"D252",
-  "productName":"NEBULIZER NE-C28 NEW",
-  "price":619500,
-  "category":"NEB"
-},
-{
-  "sku":"7727",
-  "productName":"NEBULIZER NE-C801",
-  "price":468500,
-  "category":"NEB"
-},
-{
-  "sku":"D162",
-  "productName":"NEBULIZER NE-C900",
-  "price":1243200,
-  "category":"NEB"
-},
-{
-  "sku":"B304",
-  "productName":"NEBULIZER NEU100",
-  "price":2079818,
-  "category":"NEB"
-},
-{
-  "sku":"D161",
-  "productName":"NEBULIZER NE-U780",
-  "price":8288000,
-  "category":"NEB"
-},
-{
-  "sku":"6880",
-  "productName":"PROBE COVER TH839S",
-  "price":119576,
-  "category":"PART"
-},
-{
-  "sku":"B735",
-  "productName":"PULSE MASSAGER HV-F013",
-  "price":445493,
-  "category":"TENS"
-},
-{
-  "sku":"E770",
-  "productName":"STAND KIT HEM-907",
-  "price":2690600,
-  "category":"PART"
-},
-{
-  "sku":"E245",
-  "productName":"THERMOMETER MC-246 AP4",
-  "price":56880,
-  "category":"ETF"
-},
-{
-  "sku":"E297",
-  "productName":"THERMOMETER MC-341 AP4",
-  "price":59940,
-  "category":"ETF"
-},
-{
-  "sku":"E089",
-  "productName":"THERMOMETER MC-343F",
-  "price":78220,
-  "category":"ETF"
-},
-{
-  "sku":"F139",
-  "productName":"THERMOMETER MC-F300",
-  "price":452000,
-  "category":"ETF"
-},
-{
-  "sku":"8504",
-  "productName":"WEIGHT SCALE HN 289 (HITAM)",
-  "price":227739,
-  "category":"BCM"
-},
-{
-  "sku": "8091",
-  "productName": "BODY FAT MONITOR HBF-214",
-  "price": 626316,
-  "category": "BCM"
+  "BCM": [
+    {
+      "sku": "6830",
+      "product_name": "BODY FAT MONITOR HBF-375",
+      "omron_name": "HBF-375-AP",
+      "price": 1641320
+    },
+    {
+      "sku": "8091",
+      "product_name": "KARADA SCAN HBF 214",
+      "omron_name": "HBF-214-AP",
+      "price": 626316
+    },
+    {
+      "sku": "8504",
+      "product_name": "WEIGHT SCALE HN 289 (HITAM)",
+      "omron_name": "HN-289-EBK",
+      "price": 227739
+    }
+  ],
+  "PARTS": [
+    {
+      "sku": "6865",
+      "product_name": "ADAPTOR (BPM)",
+      "omron_name": "ADAPTOR (BPM)",
+      "price": 98011
+    },
+    {
+      "sku": "6861",
+      "product_name": "CUFF - S HEM-CS24-BAP",
+      "omron_name": "HEM-CS24-AP2",
+      "price": 93790
+    },
+    {
+      "sku": "6862",
+      "product_name": "CUFF - M HEM-CR24-BAP",
+      "omron_name": "HEM-CR24-AP2",
+      "price": 105514
+    },
+    {
+      "sku": "6863",
+      "product_name": "CUFF - L HEM-RML31-BAP",
+      "omron_name": "HEM-RML31-AP2",
+      "price": 140491
+    },
+    {
+      "sku": "F275",
+      "product_name": "OMRON MANSET HEM-FL31",
+      "omron_name": "HEM-FL31-AP2",
+      "price": 187000
+    },
+    {
+      "sku": "X013",
+      "product_name": "PART1",
+      "omron_name": "9063677-9",
+      "price": 115500
+    },
+    {
+      "sku": "X014",
+      "product_name": "PART2",
+      "omron_name": "9956268-9",
+      "price": 128960
+    },
+    {
+      "sku": "X015",
+      "product_name": "PART3",
+      "omron_name": "9956276-0",
+      "price": 31557
+    },
+    {
+      "sku": "X018",
+      "product_name": "Part4",
+      "omron_name": "HHP-CM01-AP",
+      "price": 94669
+    }
+  ],
+  "NEBULIZER": [
+    {
+      "sku": "4682",
+      "product_name": "NEBULIZER NE-C28",
+      "omron_name": "NE-C28-C1",
+      "price": 619500
+    },
+    {
+      "sku": "7727",
+      "product_name": "NEBULIZER NE-C801",
+      "omron_name": "NE-C810-AP",
+      "price": 468430
+    },
+    {
+      "sku": "B292",
+      "product_name": "NEBULIZER NEC 101",
+      "omron_name": "NE-C101-AP",
+      "price": 429250
+    },
+    {
+      "sku": "X008",
+      "product_name": "Nebulizer NE-U200",
+      "omron_name": "NE-U200(NE-U200-AP)",
+      "price": 704622
+    },
+    {
+      "sku": "X009",
+      "product_name": "Nebulizer NE-U300",
+      "omron_name": "NE-U300-AP",
+      "price": 422459
+    }
+  ],
+  "BPM": [
+    {
+      "sku": "8085",
+      "product_name": "BLOOD PRESSURE MONT HEM 7120",
+      "omron_name": "HEM-7120-AP",
+      "price": 408000
+    },
+    {
+      "sku": "B813",
+      "product_name": "BLOOD PRESSURE MONT HEM 7156AAP",
+      "omron_name": "HEM-7156-AAP",
+      "price": 685000
+    },
+    {
+      "sku": "D239",
+      "product_name": "BLOOD PRESSURE MONT HEM 7156T-A",
+      "omron_name": "HEM-7156T-AAP",
+      "price": 783000
+    },
+    {
+      "sku": "D526",
+      "product_name": "BLOOD PRESSURE MONT HEM 7156T",
+      "omron_name": "HEM-7156T-AP",
+      "price": 619500
+    },
+    {
+      "sku": "E250",
+      "product_name": "BLOOD PRESSURE MONT HEM 7156-AP",
+      "omron_name": "HEM-7156-AP",
+      "price": 577280
+    },
+    {
+      "sku": "E457",
+      "product_name": "BLOOD PRESSURE MONT+ECG HEM-7530T",
+      "omron_name": "HEM-7530T-AP3",
+      "price": 2492440
+    },
+    {
+      "sku": "E658",
+      "product_name": "BLOOD PRESSURE MONT HEM-7142T2",
+      "omron_name": "HEM-7142T2-AP2",
+      "price": 412000
+    },
+    {
+      "sku": "E670",
+      "product_name": "BLOOD PRESSURE MONT JPN-750",
+      "omron_name": "HEM-7157-AP3[JPN750]",
+      "price": 702512
+    },
+    {
+      "sku": "F140",
+      "product_name": "BLOOD PRESSURE MONT HEM-7383T1",
+      "omron_name": "HEM-7383T1-AP",
+      "price": 1230000
+    },
+    {
+      "sku": "X002",
+      "product_name": "Tensimeter 7180",
+      "omron_name": "HEM-7180-AP",
+      "price": 395455
+    },
+    {
+      "sku": "X003",
+      "product_name": "Tensimeter 7183",
+      "omron_name": "HEM-7183-AP",
+      "price": 413636
+    },
+    {
+      "sku": "X004",
+      "product_name": "Tensimeter 7183C",
+      "omron_name": "HEM-7183-CAP",
+      "price": 515909
+    },
+    {
+      "sku": "X005",
+      "product_name": "Tensimeter 7183L",
+      "omron_name": "HEM-7183-LAP",
+      "price": 459091
+    },
+    {
+      "sku": "X006",
+      "product_name": "Tensimeter 7183FL",
+      "omron_name": "HEM-7193-FLAP",
+      "price": 659091
+    },
+    {
+      "sku": "X007",
+      "product_name": "Tensimeter 7194T1FL",
+      "omron_name": "HEM-7194T1-FLAP",
+      "price": 732955
+    },
+    {
+      "sku": "B438",
+      "product_name": "BLOOD PRESSURE MONITOR HEM-6232T",
+      "omron_name": "HEM-6232T-AP",
+      "price": 1108010
+    }
+  ],
+  "EFT": [
+    {
+      "sku": "8375",
+      "product_name": "EAR THERMOMETER TH-8395",
+      "omron_name": "MC-523-AP",
+      "price": 452153
+    },
+    {
+      "sku": "E089",
+      "product_name": "THERMOMETER MC-343F",
+      "omron_name": "MC-343F-AP4",
+      "price": 78220
+    },
+    {
+      "sku": "E245",
+      "product_name": "THERMOMETER MC-246 AP4",
+      "omron_name": "MC-246-AP4",
+      "price": 56880
+    },
+    {
+      "sku": "E297",
+      "product_name": "THERMOMETER MC-341 AP4",
+      "omron_name": "MC-341-AP4",
+      "price": 59940
+    },
+    {
+      "sku": "F139",
+      "product_name": "THERMOMETER MC-F300",
+      "omron_name": "MC-F300-AP",
+      "price": 452000
+    }
+  ],
+  "TENS": [
+    {
+      "sku": "B735",
+      "product_name": "PULSE MASSAGER HV-F013",
+      "omron_name": "HV-F013-AP4",
+      "price": 445493
+    },
+    {
+      "sku": "X010",
+      "product_name": "Pulse Massager HV-F021",
+      "omron_name": "HV-F021-AP",
+      "price": 856780
+    },
+    {
+      "sku": "X011",
+      "product_name": "Pulse Massager HV-F030",
+      "omron_name": "HV-F030-AP",
+      "price": 751648
+    },
+    {
+      "sku": "X012",
+      "product_name": "Pulse Massager HV-F230",
+      "omron_name": "HV-F230-AP",
+      "price": 822189
+    },
+    {
+      "sku": "X019",
+      "product_name": "Pulse Massager F-021",
+      "omron_name": "HV-F021-AP",
+      "price": 856781
+    }
+  ],
+  "NEBU": [
+    {
+      "sku": "D252",
+      "product_name": "NEBULIZER NE-C28 NEW",
+      "omron_name": "NE-C28-C1",
+      "price": 619500
+    }
+  ],
+  "HOSPITAL": [
+    {
+      "sku": "D161",
+      "product_name": "NEBULIZER NE-U780",
+      "omron_name": "NE-U780-AP",
+      "price": 8288289
+    },
+    {
+      "sku": "D161",
+      "product_name": "NEBULIZER NE-U780",
+      "omron_name": "NE-U780-AP2",
+      "price": 8288289
+    },
+    {
+      "sku": "D162",
+      "product_name": "NEBULIZER NE-C900",
+      "omron_name": "NE-C900-AP",
+      "price": 1243243
+    },
+    {
+      "sku": "D163",
+      "product_name": "BLOOD PRESSURE MONT HEM 907",
+      "omron_name": "HEM-907C3",
+      "price": 7770270
+    },
+    {
+      "sku": "D165",
+      "product_name": "BLOOD PRESSURE MONT HBP1320",
+      "omron_name": "HBP-1320-AP",
+      "price": 3108108
+    },
+    {
+      "sku": "D166",
+      "product_name": "BLOOD PRESSURE MONT HBP1120",
+      "omron_name": "HBP-1120-AP",
+      "price": 1554055
+    },
+    {
+      "sku": "E040",
+      "product_name": "BLOOD PRESSURE MONT HBP9031C",
+      "omron_name": "HBP-9031C",
+      "price": 27454955
+    },
+    {
+      "sku": "X016",
+      "product_name": "Tensimeter 9010",
+      "omron_name": "HBP-9010C-AP",
+      "price": 16527240
+    },
+    {
+      "sku": "X017",
+      "product_name": "Tensimeter 9030",
+      "omron_name": "HBP-9030",
+      "price": 26418919
+    }
+  ]
 }
-]
